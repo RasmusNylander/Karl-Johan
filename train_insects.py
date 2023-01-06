c6ef86dc0640961007ec630840f62b3a2408e306
Rasmus Nylander <43042651+RasmusNylander@users.noreply.github.com>
2023-01-06 10:23:45 +0100

    train_insects: Refactor `train`
    This removes the use of global variables and replaces the python list
    with a tensor thereby staying within the domain of torch.

--- train_insects.py
+++ train_insects.py
@@ -2,27 +2,23 @@ from monai.data import DataLoader
 from torch.nn.modules.loss import _Loss
 from torch.optim import Optimizer
 import torch
-from torch import device as Device
+from torch import Tensor, device as Device
 
 
-def train(model, dataloader: DataLoader, loss_function: _Loss, optimizer: Optimizer, device: Device, writer) -> float:
-	total_loss = []
-	global iteration
-
+def train_one_epoch(model, dataloader: DataLoader, loss_function: _Loss, optimizer: Optimizer, device: Device, writer, logging_offset: int) -> float:
 	model.train()
-	for batch_idx, (inputs, targets) in enumerate(dataloader):
+	num_training_batches = len(dataloader)
+	train_loss: Tensor = torch.empty(num_training_batches, device=device)
+	for batch_index, (inputs, targets) in enumerate(dataloader):
 		optimizer.zero_grad()
-		outputs = model(inputs.to(device))
+		outputs: Tensor = model(inputs.to_device(device))
+		targets: Tensor = targets.squeeze(1).long().to(device)
 
-		targets = torch.squeeze(targets, 1).long().to(device)
 		loss = loss_function(outputs, targets)
-
-		total_loss.append(loss.item())
-		writer.add_scalar('train_loss_logs', loss.item(), iteration)
-		iteration += 1
+		train_loss[batch_index] = loss.item()
+		writer.add_scalar('train_loss_logs', loss.item(), logging_offset + batch_index)
 
 		loss.backward()
 		optimizer.step()
 
-	epoch_loss = sum(total_loss) / len(total_loss)
-	return epoch_loss
+	return sum(train_loss) / len(train_loss)
