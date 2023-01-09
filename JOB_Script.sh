8381d98cfee4fb4c9870e6bb32a94de001c4c574
SelmaMeyer <32373064+SelmaMeyer@users.noreply.github.com>
2023-01-09 13:51:05 +0100

    Update JOB_Script.sh

--- JOB_Script.sh
+++ JOB_Script.sh
@@ -17,6 +17,6 @@ module load numpy/1.23.3-python-3.10.7-openblas-0.3.21
 module load pandas/1.4.4-python-3.10.7
 
 source Karl-Johan/env/bin/activate
-pip install -r Karl-Johan/requirement.txt --extra-index-url https://download.pytorch.org/whl/cu117
+pip install -r Karl-Johan/requirements.txt --extra-index-url https://download.pytorch.org/whl/cu117
 
 python Karl-Johan/train_insects.py
