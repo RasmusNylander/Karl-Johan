ec6df5bcbb2a0581c60dca2835fc1b7ea73cc849
Rasmus Nylander <43042651+RasmusNylander@users.noreply.github.com>
2023-01-10 09:10:07 +0100

    dataset: Add `make_dataset_csv` with creates a csv-file containing all the file in the dataset

---
+++ datasets/make_dataset_csv.py
@@ -0,0 +1,10 @@
+import os
+
+DATASET_PATH = "./sorted_downscaled"
+
+folders = [name for name in os.listdir(DATASET_PATH) if os.path.isdir(os.path.join(DATASET_PATH, name))]
+files = [f"{folder}/{file}" for folder in folders for file in os.listdir(f"{DATASET_PATH}/{folder}")]
+# write files as csv
+with open(f"{DATASET_PATH}/files.csv", "w") as f:
+    f.write("files\n")
+    f.write("\n".join(files))
