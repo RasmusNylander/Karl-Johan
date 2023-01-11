7b1a8864243e13718b3dfdd561906cdc1506b548
Rasmus Nylander <43042651+RasmusNylander@users.noreply.github.com>
2023-01-11 14:19:18 +0100

    generate_attentionmaps: Make model and scale specified from commandline

--- generate_attentionmaps.py
+++ generate_attentionmaps.py
@@ -1,3 +1,4 @@
+import argparse
 import copy
 import os
 
@@ -11,13 +12,6 @@ from create_dataloader import Dataset, make_dataloaders
 from medcam import medcam
 from model_picker import ModelType, get_model
 
-model_type: ModelType = ModelType.ResNet18
-scale: float = 0.5
-assert scale in [0.25, 0.5, 1.0]
-
-
-MODELS_ROOT = "./models"
-DATA_PATH = "./datasets/sorted_downscaled"
 BATCH_SIZE = 1
 assert BATCH_SIZE == 1
 
@@ -27,47 +21,78 @@ def save_attention_map(attention_map: Tensor, path: str):
     image_nifti = nib.Nifti1Image(first_channel, affine=np.eye(4))
     nib.save(image_nifti, path)
 
-model_string_id = f"{model_type.name}_{str(int(scale * 100)).zfill(3)}"
+def generate_attention_maps(
+    model_type: ModelType,
+    scale: float,
+    models_root: str,
+    data_path: str,
+):
+    model_string_id = f"{model_type.name}_{str(int(scale * 100)).zfill(3)}"
 
-model_path = f"{MODELS_ROOT}/{model_string_id}.pth"
+    model_path = f"{models_root}/{model_string_id}.pth"
 
-device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-# device = torch.device("cpu")
-_, _, test_loader = make_dataloaders(num_workers=0, persistent_workers=False, data_path=DATA_PATH,
-                                     batch_size=BATCH_SIZE, scale=scale)
-dataset: Dataset = test_loader.dataset
-num_classes = len(dataset.get_image_classes())
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    device = torch.device("cpu")
+    _, _, test_loader = make_dataloaders(num_workers=0, persistent_workers=False, data_path=data_path,
+                                         batch_size=BATCH_SIZE, scale=scale)
 
-model = get_model(model_type)
-model.eval()
-model.to(device)
-model.load_state_dict(torch.load(model_path, map_location=device)['net'], strict=True)
+    dataset: Dataset = test_loader.dataset
 
-models = [copy.copy(model)]
-medcam.inject(models[0], return_attention=True, layer="auto", label="best")
-for label in range(dataset.num_classes()):
-    medcam_model = copy.copy(model)
-    medcam_model = medcam.inject(medcam_model, return_attention=True, layer="auto", label=label)
-    models.append(medcam_model)
+    model = get_model(model_type)
+    model.eval()
+    model.to(device)
+    model.load_state_dict(torch.load(model_path, map_location=device)['net'], strict=True)
 
-image_output_root = f"attention_maps/{model_string_id}/layer"
-assert BATCH_SIZE == 1
-for image_id, (image_batch, batch_labels) in enumerate(tqdm(test_loader, unit="image")):
-    image_name = dataset.get_name_of_image(image_id)
-    image_dir = f"{image_output_root}/{image_name}"
-    if not os.path.exists(image_dir):
-        os.makedirs(image_dir)
+    models = [copy.copy(model)]
+    medcam.inject(models[0], return_attention=True, layer="auto", label="best")
+    for label in range(dataset.num_classes()):
+        medcam_model = copy.copy(model)
+        medcam_model = medcam.inject(medcam_model, return_attention=True, layer="auto", label=label)
+        models.append(medcam_model)
 
-    image_batch = image_batch.to(device)
+    image_output_root = f"attention_maps/{model_string_id}/layer"
+    assert BATCH_SIZE == 1
+    for image_id, (image_batch, batch_labels) in enumerate(tqdm(test_loader, unit="image")):
+        image_name = dataset.get_name_of_image(image_id)
+        image_dir = f"{image_output_root}/{image_name}"
+        if not os.path.exists(image_dir):
+            os.makedirs(image_dir)
 
-    prediction, attention_map_predicted_label = models[0](image_batch)
-    prediction_label = prediction[0].argmax(dim=0).item()
-    save_attention_map(attention_map_predicted_label[0].detach().cpu(),
-                       f"{image_dir}/{dataset.label_to_name(prediction_label)}")
+        image_batch = image_batch.to(device)
 
-    correct_label = batch_labels[0].argmax(dim=0).item()
-    if correct_label == prediction_label:
-        continue
-    _, attention_map_correct_label = models[correct_label + 1](image_batch)
-    save_attention_map(attention_map_correct_label[0].detach().cpu(),
-                       f"{image_dir}/{dataset.label_to_name(correct_label)}")
+        prediction, attention_map_predicted_label = models[0](image_batch)
+        prediction_label = prediction[0].argmax(dim=0).item()
+        save_attention_map(attention_map_predicted_label[0].detach().cpu(),
+                           f"{image_dir}/{dataset.label_to_name(prediction_label)}")
+
+        correct_label = batch_labels[0].argmax(dim=0).item()
+        if correct_label == prediction_label:
+            continue
+        _, attention_map_correct_label = models[correct_label + 1](image_batch)
+        save_attention_map(attention_map_correct_label[0].detach().cpu(),
+                           f"{image_dir}/{dataset.label_to_name(correct_label)}")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Generate attention maps for a model")
+    parser.add_argument("--data_path", default="./datasets/sorted_downscaled", type=str)
+    parser.add_argument("--models_root", default="./models", type=str)
+    parser.add_argument("--output_path", default="./attention_maps", type=str)
+    parser.add_argument("--model", type=str, help='ResNet18, ResNet50, DenseNet, SEResNet50', required=True)
+    parser.add_argument("--scale", type=float, help='Scale of the images. Must be 0.25, 0.5 or 1.0', required=True)
+
+    args = parser.parse_args()
+    data_path = args.data_path
+    models_root = args.models_root
+    output_path = args.output_path
+
+    scale = args.scale
+    assert scale in [0.25, 0.5, 1.0], f"scale of {scale} not yet supported. Scale must be either 0.25, 0.5 or 1.0"
+
+    model_name = args.model
+    try:
+        model_type = ModelType[model_name]
+    except KeyError:
+        raise ValueError(f"Model {model_name} not supported. Choose from {[type.name for type in ModelType]}")
+
+    generate_attention_maps(model_type, scale, models_root, data_path)
