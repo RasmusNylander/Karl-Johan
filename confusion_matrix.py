2449863cd4f24ad4bc80b8cd7709c615153ada35
Rasmus Nylander <43042651+RasmusNylander@users.noreply.github.com>
2023-01-13 11:35:37 +0100

    confusion_matrix: Fix use of dataloader

--- confusion_matrix.py
+++ confusion_matrix.py
@@ -1,31 +1,28 @@
-import os
-import sys
-import time
-import argparse
-
 import torch
-from torch import Tensor, device as Device
+from tqdm.contrib import itertools
 
-from create_dataloader import DatasetScale, make_dataloaders
-from model_picker import ModelType, get_model
+
+from create_dataloader import DatasetScale, MNInSecTVariant, make_dataloaders
+from model_picker import ModelType, get_model, get_model_name, get_pretrained
 from torchmetrics import ConfusionMatrix
 import numpy as np
 import tqdm
 
-def create_confusion(model,data_loader):
-    all_labels = []
-    all_outputs = []
+def create_confusion(model, data_loader):
+    images_processed = 0
+    targets = torch.empty(len(data_loader.dataset))
+    predictions = torch.empty(len(data_loader.dataset))
     model.eval()
     with torch.no_grad():
-        for image_batch, labels in tqdm.tqdm(data_loader,unit="batch"):
+        for image_batch, labels in tqdm.tqdm(data_loader, unit="batch", leave=False):
             output = model(image_batch.to(device))
-            all_labels.append(labels)
-            all_outputs.append(output.cpu())
-    targets = torch.vstack(all_labels).argmax(dim=1)
-    preds = torch.vstack(all_outputs).argmax(dim=1)
+            targets[images_processed:images_processed+len(image_batch)] = labels.argmax(dim=1)
+            predictions[images_processed:images_processed+len(image_batch)] = output.argmax(dim=1).cpu()
+            images_processed += len(image_batch)
+
     
     confmat = ConfusionMatrix(task="multiclass", num_classes=10)
-    cm = confmat(preds,targets)
+    cm = confmat(predictions, targets)
     return cm
             
 
@@ -34,25 +31,22 @@ if __name__=="__main__":
     MODELS_ROOT = "./models"
     DATA_PATH = "./datasets/MNInSecT"
 
-    for model_type in ModelType:
-        for scale in DatasetScale:
-            model_path = f"{MODELS_ROOT}/{model_type.name}_{str(int(scale.to_float()*100)).zfill(3)}.pth"
+    for model_type, scale, variant in itertools.product(ModelType, DatasetScale, MNInSecTVariant):
+        model_name = get_model_name(model_type, variant, scale)
 
-            device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-            model = get_model(model_type)
-            model.to(device)
-            model.load_state_dict(torch.load(model_path, map_location=device)['net'], strict=True)
+        device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+        model = get_pretrained(model_type, variant, scale, MODELS_ROOT, map_location=device).to(device)
 
-            _,_,test_loader = make_dataloaders(
-                batch_size=10,
-                seed=69420,
-                data_path=DATA_PATH,
-                pin_memory=False,
-                as_rgb=False,
-                scale=scale,
-                masked = True
-            )
-    
-            confmat = create_confusion(model,test_loader)
+        _,_,test_loader = make_dataloaders(
+            batch_size=10,
+            seed=69420,
+            data_path=DATA_PATH,
+            pin_memory=False,
+            as_rgb=False,
+            scale=scale,
+            variant=variant,
+        )
 
-            np.save(f"confusion_matrix/{model_type.name}_{str(int(scale.to_float()*100)).zfill(3)}",confmat.numpy())
+        confmat = create_confusion(model, test_loader)
+
+        np.save(f"confusion_matrix/{model_name}", confmat.numpy())
