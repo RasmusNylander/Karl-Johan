8463ef9edcdef9df35f31e663a1453ab7e6bda4f
Rasmus Nylander <43042651+RasmusNylander@users.noreply.github.com>
2023-01-13 12:11:26 +0100

    paraview_test: Threshold image and attention_mao

--- paraview_test.py
+++ paraview_test.py
@@ -11,12 +11,14 @@ from model_picker import ModelType, get_model_name
 
 MODELS_ROOT = "./models"
 DATA_PATH = "./datasets/MNInSecT/"
+ATTENTION_MAPS_ROOT = "D:/attention_maps"
 BATCH_SIZE = 1
 assert BATCH_SIZE == 1
 
 model_type: ModelType = ModelType.ResNet18
 scale: DatasetScale = DatasetScale.Scale100
 dataset_variant: MNInSecTVariant = MNInSecTVariant.Original
+layer = 2
 
 model_string_id = get_model_name(model_type, dataset_variant, scale)
 
@@ -26,18 +28,19 @@ dataset: Dataset = Dataset(MNInSecT_root=DATA_PATH, type=DatasetType.Test, seed=
 
 image, batch_labels = dataset[0]
 image_name = dataset.get_name_of_image(0)
-layer = 3
 
-attentionmap_paths = glob.glob(f"./attention_maps/{model_string_id}/layer{layer}/{image_name}/*")
+attentionmap_paths = glob.glob(f"{ATTENTION_MAPS_ROOT}/{model_string_id}/layer{layer}/{image_name}/*")
 prediction_path = [path for path in attentionmap_paths if "prediction" in path][0]
 prediction_map = np.array(nib.load(prediction_path).dataobj).transpose(2, 0, 1)
 prediction_map = torch.from_numpy(prediction_map)
 
 attention_map = F.interpolate(prediction_map.unsqueeze(dim=0).unsqueeze(dim=0), image.squeeze().shape)[0]
 attention_map = attention_map[0]
+attention_map[attention_map < 0.2] = 0
 
 image = image / image.max()
 image = image[0]
+image[image < 0.1] = 0
 
 background = (attention_map == 0) & (image == 0)
 opacity = torch.zeros(image.shape)
