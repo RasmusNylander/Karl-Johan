4b4b770fb0fb5580428e76348aaa93bf68571ba6
Rasmus Nylander <43042651+RasmusNylander@users.noreply.github.com>
2023-01-13 10:38:14 +0100

    model_picker: Add function `get_pretrained` and `get_model_name`

--- model_picker.py
+++ model_picker.py
@@ -2,6 +2,8 @@ from enum import Enum, auto
 import torch
 from monai.networks.nets import densenet121, SEResNet50, ResNet
 
+from create_dataloader import DatasetScale, MNInSecTVariant
+
 
 class ModelType(Enum):
     ResNet18 = auto(),
@@ -20,4 +22,18 @@ def get_model(type: ModelType) -> torch.nn.Module:
         case ModelType.SEResNet50:
             return SEResNet50(spatial_dims=3, in_channels=1, num_classes=10)
 
-# def get_pretrained(type: ModelType, )
+
+def get_model_name(type: ModelType, dataset_variant: MNInSecTVariant, scale: DatasetScale) -> str:
+    assert len(ModelType) == 4 and len(DatasetScale) == 3 and len(MNInSecTVariant) == 2
+
+    scale_as_string = str(int(scale.to_float() * 100)).zfill(3)
+    scale_suffix = f"_{scale_as_string}"
+    variant_suffix = "_masked" if dataset_variant == MNInSecTVariant.Masked else ""
+    return f"{type.name}{scale_suffix}{variant_suffix}"
+
+def get_pretrained(type: ModelType, dataset_variant: MNInSecTVariant, scale: DatasetScale, models_root: str, map_location=None) -> torch.nn.Module:
+    name = get_model_name(type, dataset_variant, scale)
+    model_path = f"{models_root}/{name}.pth"
+    model = get_model(type)
+    model.load_state_dict(torch.load(model_path, map_location=map_location)['net'], strict=True)
+    return model
