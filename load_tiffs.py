448309125d47846da963d2a5bd49086cc18ca4a1
Oisín Hageman <O.Hageman@student.tudelft.nl>
2023-01-13 16:14:24 +0100

    load_tiffs: load arbitrary number of tif files and set multi component mapping automatically

--- load_tiffs.py
+++ load_tiffs.py
@@ -1,13 +1,17 @@
 from paraview.simple import *
 import os
+import glob
 
-TIFF_ROOT = "/Karl-Johan"  # Just put the absolute path to Karl-Johan
+TIFF_ROOT = "C:/Users/oisin/Documents/Uni/DTU/02507/repo/Karl-Johan"
 
-reader = TIFFReader(FileName=f"{TIFF_ROOT}/temp_image.tif")
-disp = GetDisplayProperties()
-disp.Representation = "Volume"
+file_names = glob.glob(f"{TIFF_ROOT}/*.tif")
+for file_name in file_names:
+    reader = TIFFReader(FileName=file_name)
+    disp = GetDisplayProperties()
+    disp.Representation = "Volume"
 
-display = Show(reader)
-ColorBy(display, ('POINTS', 'Tiff Scalars'))  # Sets the correct data to be used for coloring
+    display = Show(reader)
+    ColorBy(display, ('POINTS', 'Tiff Scalars'))  # Sets the correct data to be used for coloring
+    disp.MultiComponentsMapping = True
 
 Render()
